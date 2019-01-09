a2e80b98ceb612ba1b8c945e0d8cc2a065aa628c
tetsuo <you@example.com>
2019-01-09 17:59:30 +0900

    タグ検索を実装

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,11 @@
 class ToppagesController < ApplicationController
   def index
-    @posts = Post.page(params[:page]).per(5).order('created_at desc')
+    if params[:tag_id]
+      @tag = Tag.find(params[:tag_id])
+      @posts = @tag.posts.page(params[:page]).per(5).order('created_at desc')
+    else
+      @posts = Post.page(params[:page]).per(5).order('created_at desc')
+    end
     @tags = Tag.all
     if user_signed_in?
       @user = current_user
