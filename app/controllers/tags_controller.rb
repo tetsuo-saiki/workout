91bb3ef19bc33785b5be8bc404f9e26b2fbefb74
tetsuo <you@example.com>
2019-01-09 12:43:23 +0900

    タグ作成機能追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,34 @@
 class TagsController < ApplicationController
   def new
+    if user_signed_in?
+      @user = current_user
+      @default_tags = Tag.all.order('created_at asc').limit(15)
+      @user_tags = Tag.where('user_id', current_user.id).page(params[:page]).per(5).order('created_at desc')
+      @tag = Tag.new
+    end
   end
 
   def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      flash[:notice] = 'タグを作成しました。'
+      redirect_to new_tag_path
+    else
+      @user = current_user
+      @default_tags = Tag.all.order('created_at asc').limit(15)
+      @user_tags = Tag.where('user_id', current_user.id).page(params[:page]).per(5).order('created_at desc')
+      @tag = Tag.new
+      flash.now[:alert] = 'タグの作成に失敗しました。'
+      render 'tags/new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:tag, :user_id)
   end
 end
